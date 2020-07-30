99d82d50f3435e2f09108f483445297c4d752eeb
김형태 선임 카드캐피탈사업팀 (kkimtae@lgcns.com <kkimtae@CNS.LGCNS.COM>
2020-07-30 13:19:43 +0900

    settings change

--- src/main/java/me/ht/edu/Application.java
+++ src/main/java/me/ht/edu/Application.java
@@ -8,12 +8,27 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
 	@GetMapping("/hello")
 	public String hello() {
 		return "hello string";
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+//	@Bean
+//	public ServletWebServerFactory serverFactory() {
+//		TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+//		tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+//
+//		return tomcat;
+//	}
+//
+//	private Connector createStandardConnector() {
+//		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//		connector.setPort(8080);
+//		return connector;
+//	}
+
 }
