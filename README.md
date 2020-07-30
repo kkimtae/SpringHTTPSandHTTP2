f6fd3dbbb92cb8d09bef0bfce5455827729da5e2
김형태 선임 카드캐피탈사업팀 (kkimtae@lgcns.com <kkimtae@CNS.LGCNS.COM>
2020-07-30 10:45:59 +0900

    https 설정

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Spring Web HTTPS and HTTP2
+curl로 서버 동작 확인 curl -I -k --http2 http(s)://localhost:8088/hello<br>
+-k 는 공인받은 인증서가 아니라 개인생성한 인증서 사용의 경우 주는 옵션
--- src/main/java/me/ht/edu/Application.java
+++ src/main/java/me/ht/edu/Application.java
@@ -2,11 +2,17 @@ package me.ht.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello string";
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
